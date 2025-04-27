adc7ed57e6974ad410525bac7abe04567be1382c
hulgee209 <batkhulege@gmail.com>
2025-04-28 00:48:46 +0800

    lab15

---
+++ README.md
@@ -0,0 +1,13 @@
+# Calculator Project
+
+Энэ бол Maven-д суурилсан Java 17 төсөл юм.  
+JUnit тест, Checkstyle шалгалт, JaCoCo-ийн 100% branch coverage-ийг агуулсан.
+
+## Ашиглах заавар:
+
+Төслийг угсрах болон шалгах командууд:
+
+```bash
+mvn clean test
+mvn checkstyle:check
+mvn jacoco:report
---
+++ src/test/java/lab15/sict/must/edu/mn/MultiplicationTest.java
@@ -0,0 +1,19 @@
+package lab15.sict.must.edu.mn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MultiplicationTest {
+
+    @Test
+    void testMultiplyPositive() {
+        Multiplication calc = new Multiplication();
+        assertEquals(6.0, calc.multiply(2.0, 3.0), "2 * 3 нь 6 байх ёстой");
+    }
+
+    @Test
+    void testMultiplyNegative() {
+        Multiplication calc = new Multiplication();
+        assertEquals(-6.0, calc.multiply(-2.0, 3.0), "-2 * 3 нь -6 байх ёстой");
+    }
+}
